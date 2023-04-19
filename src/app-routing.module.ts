cf62c3c0e7cfbcd8dca025893f82d205910e5e32
yerke <zat.tyme.su@gmail.com>
2023-04-20 03:22:28 +0600

    app routing

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HomePageComponent} from "./app/home-page/home-page.component";
+import {CompanyVacanciesComponent} from "./app/company-vacancies/company-vacancies.component";
 
 
+const routes: Routes = [
+  { path: 'companies', component: HomePageComponent },
+  { path: 'companies/:id', component: CompanyVacanciesComponent},
+  { path: '', redirectTo: '/companies', pathMatch: 'full'},
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
