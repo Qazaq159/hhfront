f42e17a8fb6c4ec99d6202830f741821c7771907
yerke <zat.tyme.su@gmail.com>
2023-04-20 04:07:32 +0600

    routes

--- hh-front/src/app/app-routing.module.ts
+++ hh-front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompaniesComponent} from "./companies/companies.component";
+import {CompanyVacanciesComponent} from "./company-vacancies/company-vacancies.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'companies', component: CompaniesComponent },
+  { path: 'companies/:id', component: CompanyVacanciesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
